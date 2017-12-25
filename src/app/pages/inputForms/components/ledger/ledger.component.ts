0a68a062cb505a6a4f3d227a42da4b1758c80b51
aadii104 <sharmaadi104@gmail.com>
2017-12-25 13:43:51 +0530

    ledger page without undergroup done

--- src/app/pages/inputForms/components/ledger/ledger.component.ts
+++ src/app/pages/inputForms/components/ledger/ledger.component.ts
@@ -18,11 +18,6 @@ export class LedgerComponent implements OnInit {
     form: FormGroup;
     selectedIndex = 1;
 
-    contraNumber: string;
-    account: string;
-    chequeNumber: string;
-    drawnBank: string;
-
     paramId: string;
 
     constructor(
@@ -37,24 +32,25 @@ export class LedgerComponent implements OnInit {
 
     ngOnInit() {
         this.form = this.fb.group({
-            contraNumber: [''],
-            date: [''],
-            account: [''],
-            chequeNumber: [''],
-            drawnBank: [''],
-            particularsData: this.fb.array([]),
-            file: [""],
-            xDate: [null, Validators.required],
-            yDate: [null, Validators.required],
-        });
-        this.addParticular();
-    }
+            ledgerName: [''],
+            applicableTax: [''],
+            BusinessType: [''],
+            gstin: [''],
+            pan: [''],
+            name:[''],
+            address: [''],
+            city: [''],
+            state: [''],
+            country: [''],
+            pinCode: [''],
+            phoneNumber: [''],
+            email: [''],
+            qty: [''],
+            rate: [''],
+            total: ['']
 
-    // real date picker active from here
-    public myDatePickerOptions: IMyDpOptions = {
-        // other options...
-        dateFormat: 'dd.mm.yyyy',
-    };
+        });
+    }
 
     public items: Array<string> = ['cash in hand(dr)', 'cash at bank(dr)', 'sales a / c(cr)', 'purchases a / c(dr)', 'stock in hand(dr)',
         ' sundry debtors(dr)', 'sundry creditors(cr)', 'current asset(dr)', 'current liabilities(cr)', 'non - current assets(dr)',
@@ -112,46 +108,6 @@ export class LedgerComponent implements OnInit {
             });
     }
 
-
-
-
-    initParticular() {
-        return this.fb.group({
-            particulars: ['', Validators.required],
-            amount: ['']
-        })
-    }
-    addParticular() {
-        const control = <FormArray>this.form.controls['particularsData'];
-        const addCtrl = this.initParticular();
-        control.push(addCtrl);
-    }
-    removeParticular(i: number) {
-        const control = <FormArray>this.form.controls['particularsData'];
-        control.removeAt(i);
-    }
-
-
-
-    // file upload code here
-    handleFileUpload(event) {
-        var file: File = event.target.files[0];
-        let valid: boolean;
-
-        // valid = this.fileValidator.isValidLogo(file);
-        // if (valid && file.size < 200000) {
-        //     this.fileValidator.checkPixel(file, (value) => {
-        //         if (value) {
-        //             this.file_size = false;
-        //             this.file_view = true;
-        //             this.file = value;
-        //         }
-        //     });
-        // }
-        // else {
-        //     this.file_size = true;
-        // }
-    }
 
     setSelected(id: number) {
         this.selectedIndex = id;
