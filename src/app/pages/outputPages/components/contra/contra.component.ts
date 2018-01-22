056dd294fabdafbd69bc98be52c90916de8ba1ed
aadii104 <sharmaadi104@gmail.com>
2018-01-22 12:22:19 +0530

    conta page 75%

--- src/app/pages/outputPages/components/contra/contra.component.ts
+++ src/app/pages/outputPages/components/contra/contra.component.ts
@@ -1,184 +1,93 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, ViewChild,  OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormArray, FormBuilder, Validators } from '@angular/forms';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 import { ActivatedRoute } from '@angular/router';
 import { ContraService } from "./service/contra.service";
 import { IMyDpOptions } from 'mydatepicker';
+import { BsModalComponent, BsModalBodyComponent } from "ng2-bs3-modal";
+
 declare var $: any;
 
 @Component({
     selector: 'app-contra',
+    host: { '(window:keydown)': 'hotkeys($event)' },
     templateUrl: './contra.component.html',
     styleUrls: ['./contra.component.scss']
 })
 
 
-// Index
-// declaration
-// constructor
-// ngOnInit
-// datepicker functions
-// select functions
-// Notification
-// particulars functions
-// file handler 
-// tab navigator 
-// submit functions
-
-
 export class ContraComponent implements OnInit {
 
 
+    contentId: string = "";
+    public dateFrom: Date;
+    public dateTo: Date;
+
+    @ViewChild('modal')
+    modal: BsModalComponent;
+
+    incomingData: Array<string>;
     form: FormGroup;
-    selectedIndex = 1;
-    dataCopy: any;
-    paramId: string;
-    closeResult: string;
-
-
+    public dataCopy: any;
+    public paramId: string;
+    public closeResult: string;
 
     constructor(
         private route: ActivatedRoute,
         public _contraService: ContraService,
-        public fb: FormBuilder,
-        private router: Router) {
-    }
+        public fb: FormBuilder, ) {
 
+    }
 
     ngOnInit() {
-        this.form = this.fb.group({
-            account: [''],
-            chequeNumber: [''],
-            contraNumber: [''],
-            date: [''],
-            drawnOn: [null, Validators.required],
-            drawnBank: [''],
-            file: [""],
-            narration:[""],
-            particularsData: this.fb.array([]),
-        });
-        this.addParticular();
+        this.getIncomingData();
+
+        this.modal.onClose.subscribe(this.onClose.bind(this));
+
     }
 
-    // real date picker active from here
+    hotkeys(event) {
+        if (event.keyCode == 65) {
+
+            this.modal.open();
+        }
+    }
+
+    onClose() {
+        console.log('Modal Closed');
+        this.contentId = "";
+    }
     public myDatePickerOptions: IMyDpOptions = {
         // other options...
         dateFormat: 'dd.mm.yyyy',
     };
 
-    public items: Array<string> = ['Amsterdam', 'Antwerp', 'Athens', 'Barcelona',
-        'Berlin', 'Birmingham', 'Bradford', 'Bremen', 'Brussels', 'Bucharest',
-        'Budapest', 'Cologne', 'Copenhagen', 'Dortmund', 'Dresden', 'Dublin',
-        'Düsseldorf', 'Essen', 'Frankfurt', 'Genoa', 'Glasgow', 'Gothenburg',
-        'Hamburg', 'Hannover', 'Helsinki', 'Kraków', 'Leeds', 'Leipzig', 'Lisbon',
-        'London', 'Madrid', 'Manchester', 'Marseille', 'Milan', 'Munich', 'Málaga',
-        'Naples', 'Palermo', 'Paris', 'Poznań', 'Prague', 'Riga', 'Rome',
-        'Rotterdam', 'Seville', 'Sheffield', 'Sofia', 'Stockholm', 'Stuttgart',
-        'The Hague', 'Turin', 'Valencia', 'Vienna', 'Vilnius', 'Warsaw', 'Wrocław',
-        'Zagreb', 'Zaragoza', 'Łódź'];
-
-    public value: any = {};
-    public _disabledV: string = '0';
-    public disabled: boolean = false;
-    private get disabledV(): string {
-        return this._disabledV;
+    getIncomingData() {
+        this.dataCopy = this._contraService.getIncomingData().map(
+            (response) => response.json()
+        ).subscribe(
+            (data) => {
+                console.log(data.contraData)
+                this.incomingData = data.contraData;
+            })
     }
 
-    private set disabledV(value: string) {
-        this._disabledV = value;
-        this.disabled = this._disabledV === '1';
+    editData(id) {
+        console.log(id);
+        this.contentId = id;
+        this._contraService.contentId = id;
+
     }
 
-    public selected(value: any): void {
-        console.log('Selected value is: ', value);
+    deleteData(id) {
+
+
     }
 
-    public removed(value: any): void {
-        console.log('Removed value is: ', value);
-    }
-
-    // public typed(value: any): void {
-    //     console.log('New search input: ', value);
-    // }
-
-    public refreshValue(value: any): void {
-        this.value = value;
-    }
+    copyData(id) {
 
 
-
-
-    public showNotification(from, align) {
-        const type = ['', 'info', 'success', 'warning', 'danger'];
-
-        var color = Math.floor((Math.random() * 4) + 1);
-        $.notify({
-            icon: "pe-7s-gift",
-            message: "Welcome to <b>ProWorkTree Dashboard</b> - a beautiful freebie for every web developer."
-        }, {
-                type: type[color],
-                timer: 1000,
-                placement: {
-                    from: from,
-                    align: align
-                }
-            });
-    }
-
-
-
-
-    initParticular() {
-        return this.fb.group({
-            particulars: ['', Validators.required],
-            amount: ['']
-        })
-    }
-    addParticular() {
-        const control = <FormArray>this.form.controls['particularsData'];
-        const addCtrl = this.initParticular();
-        control.push(addCtrl);
-    }
-    removeParticular(i: number) {
-        const control = <FormArray>this.form.controls['particularsData'];
-        control.removeAt(i);
-    }
-
-
-
-    // file upload code here
-    handleFileUpload(event) {
-        var file: File = event.target.files[0];
-        let valid: boolean;
-
-        // valid = this.fileValidator.isValidLogo(file);
-        // if (valid && file.size < 200000) {
-        //     this.fileValidator.checkPixel(file, (value) => {
-        //         if (value) {
-        //             this.file_size = false;
-        //             this.file_view = true;
-        //             this.file = value;
-        //         }
-        //     });
-        // }
-        // else {
-        //     this.file_size = true;
-        // }
-    }
-    
-    setSelected(id: number) {
-        this.selectedIndex = id;
-    }
-    
-    
-    onSubmit(user) {
-
-        console.log(user)
-        this._contraService.createNewEntry(user)
-            .subscribe(
-            (data) => { }
-            )
     }
 
 
--- src/app/pages/outputPages/components/contra/service/contra.service.ts
+++ src/app/pages/outputPages/components/contra/service/contra.service.ts
@@ -15,27 +15,18 @@ export class ContraService {
     token: string;
     windowStorage: any;
     _url: string;
+    contentId: string;
     
     constructor(private http: Http, private router: Router, private route: ActivatedRoute, public _inputFormService: OutputPagesService) {
         this.windowStorage = JSON.parse(window.localStorage.getItem('user'));
         this.token = this.windowStorage.token;
         this.paramCompanyName = this._inputFormService.paramCompanyName;
-        console.log(this.paramCompanyName)
+        // console.log(this.paramCompanyName)
     }
 
-    getData() {
-        this._url = `http://localhost:3000/api/uglist?token=${this.token}&&companyName=${this.paramCompanyName}`;
+    getIncomingData() {
+        this._url = `http://localhost:3000/api/contraStored?token=${this.token}&&companyName=${this.paramCompanyName}`;
         return this.http.get(this._url);
-    }
-
-    createNewEntry(user: any) {
-
-        this._url = `http://localhost:3000/api/contra?token=${this.token}&companyName=${this.paramCompanyName}`;
-        return this.http.post(this._url, user)
-            .map((res: Response) => {
-                this.result = res.json();
-                // console.log(this.result)
-            })
     }
 
 }
