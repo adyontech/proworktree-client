56bc913c7721802d358a14b52559b5eae004af57
aadii104 <sharmaadi104@gmail.com>
2018-01-22 07:43:55 +0530

    receipt get request done

--- src/app/pages/outputPages/components/receipt/receipt.component.ts
+++ src/app/pages/outputPages/components/receipt/receipt.component.ts
@@ -1,170 +1,174 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, ViewChild, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormArray, FormBuilder, Validators } from '@angular/forms';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 import { ActivatedRoute } from '@angular/router';
 import { ReceiptService } from "./service/receipt.service";
 import { IMyDpOptions } from 'mydatepicker';
+import { BsModalComponent, BsModalBodyComponent } from "ng2-bs3-modal";
+
 declare var $: any;
 
 @Component({
     selector: 'app-receipt',
+    host: { '(window:keydown)': 'hotkeys($event)' },
     templateUrl: './receipt.component.html',
     styleUrls: ['./receipt.component.scss']
 })
 
 export class ReceiptComponent implements OnInit {
+    // Models 
+    contentId: string = "";
+    public dateFrom: Date;
+    public dateTo: Date;
+    public dropdFilter: string;
 
 
+    //Modal for column hide and show
+
+    VColReceiptType: string = "ColReceiptType";
+    VColReceiptThrough: string = "ColReceiptThrough";
+    VColChequeNO: string = "ColChequeNO";
+    VColAgainst: string = "ColAgainst";
+
+    @ViewChild('modal')
+    modal: BsModalComponent;
+
+
+    @Input()
+    public ColReceiptType: Boolean = false;
+    public ColReceiptThrough: Boolean = false;
+    public ColChequeNO: Boolean = false;
+    public ColAgainst: Boolean = false;
+
+    incomingData: Array<string>;
     form: FormGroup;
-    selectedIndex = 1;
-    dataCopy: any;
-    paramId: string;
-    closeResult: string;
+    public dataCopy: any;
+    public paramId: string;
+    public closeResult: string;
+
+    dropdownList = [];
+    selectedItems = [];
+    dropdownSettings = {};
 
     constructor(
         private route: ActivatedRoute,
         public _receiptService: ReceiptService,
-        public fb: FormBuilder,
-        private router: Router) {
+        public fb: FormBuilder, ) {
+
     }
-
-
     ngOnInit() {
-        this.form = this.fb.group({
-            receiptNumber: [''],
-            date: [''],
-            account: [''],
-            receiptType: [''],
-            receiptThrough: [''],
-            chequeNumber: [''],
-            drawnOn: [null, Validators.required],
-            against: [''],
-            particularsData: this.fb.array([]),
-            narration: [''],
-            file: [""],
-        });
-        this.addParticular();
+        this.getIncomingData();
+        this.dropdownList = [
+            { "id": "ColReceiptType", "itemName": "Receipt Type" },
+            { "id": "ColReceiptThrough", "itemName": "Receipt Through" },
+            { "id": "ColChequeNO", "itemName": "Cheque Number" },
+            { "id": "ColAgainst", "itemName": "Against" },
+        ];
+        this.dropdownSettings = {
+            singleSelection: false,
+            text: "Select filter",
+            selectAllText: 'Select All',
+            unSelectAllText: 'UnSelect All',
+            enableSearchFilter: false,
+            classes: "myclass custom-class"
+        };
+
+        this.modal.onClose.subscribe(this.onClose.bind(this));
     }
 
+    hotkeys(event) {
+        if (event.keyCode == 65) {
+
+            this.modal.open();
+        }
+    }
+
+    onItemSelect(item: any): void {
+        switch (item.id) {
+            case this.VColReceiptType:
+                this.ColReceiptType = true;
+                break;
+            case this.VColReceiptThrough:
+                this.ColReceiptThrough = true;
+                break;
+            case this.VColChequeNO:
+                this.ColChequeNO = true;
+                break;
+            case this.VColAgainst:
+                this.ColAgainst = true;
+                break;
+
+        }
+        console.log(this.selectedItems);
+    }
+
+    OnItemDeSelect(item: any) {
+        switch (item.id) {
+            case this.VColReceiptType:
+                this.ColReceiptType = false;
+                break;
+            case this.VColReceiptThrough:
+                this.ColReceiptThrough = false;
+                break;
+            case this.VColChequeNO:
+                this.ColChequeNO = false;
+                break;
+            case this.VColAgainst:
+                this.ColAgainst = false;
+                break;
+        }
+        console.log(this.selectedItems);
+    }
+    onSelectAll(items: any) {
+        // console.log(items);
+        this.ColReceiptType = true;
+        this.ColReceiptThrough = true;
+        this.ColChequeNO = true;
+        this.ColAgainst = true;
+    }
+    onDeSelectAll(items: any) {
+        // console.log(items);
+        this.ColReceiptType = false;
+        this.ColReceiptThrough = false;
+        this.ColChequeNO = false;
+        this.ColAgainst = false;
+    }
+    onClose() {
+        console.log('Modal Closed');
+        this.contentId = "";
+    }
     // real date picker active from here
     public myDatePickerOptions: IMyDpOptions = {
         // other options...
         dateFormat: 'dd.mm.yyyy',
     };
 
-    public items: Array<string> = ['Amsterdam', 'Antwerp', 'Athens', 'Barcelona',
-        'Berlin', 'Birmingham', 'Bradford', 'Bremen', 'Brussels', 'Bucharest',
-        'Budapest', 'Cologne', 'Copenhagen', 'Dortmund', 'Dresden', 'Dublin',
-        'Düsseldorf', 'Essen', 'Frankfurt', 'Genoa', 'Glasgow', 'Gothenburg',
-        'Hamburg', 'Hannover', 'Helsinki', 'Kraków', 'Leeds', 'Leipzig', 'Lisbon',
-        'London', 'Madrid', 'Manchester', 'Marseille', 'Milan', 'Munich', 'Málaga',
-        'Naples', 'Palermo', 'Paris', 'Poznań', 'Prague', 'Riga', 'Rome',
-        'Rotterdam', 'Seville', 'Sheffield', 'Sofia', 'Stockholm', 'Stuttgart',
-        'The Hague', 'Turin', 'Valencia', 'Vienna', 'Vilnius', 'Warsaw', 'Wrocław',
-        'Zagreb', 'Zaragoza', 'Łódź'];
-
-    public value: any = {};
-    public _disabledV: string = '0';
-    public disabled: boolean = false;
-    private get disabledV(): string {
-        return this._disabledV;
+    getIncomingData() {
+        this.dataCopy = this._receiptService.getIncomingData().map(
+            (response) => response.json()
+        ).subscribe(
+            (data) => {
+                console.log(data)
+                this.incomingData = data.receiptData;
+            })
     }
 
-    private set disabledV(value: string) {
-        this._disabledV = value;
-        this.disabled = this._disabledV === '1';
+    editData(id) {
+        console.log(id);
+        this.contentId = id;
+        this._receiptService.contentId = id;
+
     }
 
-    public selected(value: any): void {
-        console.log('Selected value is: ', value);
+    deleteData(id) {
+
+
     }
 
-    public removed(value: any): void {
-        console.log('Removed value is: ', value);
-    }
-
-    // public typed(value: any): void {
-    //     console.log('New search input: ', value);
-    // }
-
-    public refreshValue(value: any): void {
-        this.value = value;
-    }
+    copyData(id) {
 
 
-
-
-    public showNotification(from, align) {
-        const type = ['', 'info', 'success', 'warning', 'danger'];
-
-        var color = Math.floor((Math.random() * 4) + 1);
-        $.notify({
-            icon: "pe-7s-gift",
-            message: "Welcome to <b>ProWorkTree Dashboard</b> - a beautiful freebie for every web developer."
-        }, {
-                type: type[color],
-                timer: 1000,
-                placement: {
-                    from: from,
-                    align: align
-                }
-            });
-    }
-
-
-
-
-    initParticular() {
-        return this.fb.group({
-            particulars: ['', Validators.required],
-            amount: ['']
-        })
-    }
-    addParticular() {
-        const control = <FormArray>this.form.controls['particularsData'];
-        const addCtrl = this.initParticular();
-        control.push(addCtrl);
-    }
-    removeParticular(i: number) {
-        const control = <FormArray>this.form.controls['particularsData'];
-        control.removeAt(i);
-    }
-
-
-
-    // file upload code here
-    handleFileUpload(event) {
-        var file: File = event.target.files[0];
-        let valid: boolean;
-
-        // valid = this.fileValidator.isValidLogo(file);
-        // if (valid && file.size < 200000) {
-        //     this.fileValidator.checkPixel(file, (value) => {
-        //         if (value) {
-        //             this.file_size = false;
-        //             this.file_view = true;
-        //             this.file = value;
-        //         }
-        //     });
-        // }
-        // else {
-        //     this.file_size = true;
-        // }
-    }
-
-    setSelected(id: number) {
-        this.selectedIndex = id;
-    }
-
-
-    onSubmit(user) {
-
-        console.log(user)
-        this._receiptService.createNewEntry(user)
-            .subscribe(
-            (data) => {}
-            )
     }
 
 }
--- src/app/pages/outputPages/components/receipt/service/receipt.service.ts
+++ src/app/pages/outputPages/components/receipt/service/receipt.service.ts
@@ -15,27 +15,18 @@ export class ReceiptService {
     token: string;
     windowStorage: any;
     _url: string;
+    contentId: string;
 
     constructor(private http: Http, private router: Router, private route: ActivatedRoute, public _inputFormService: OutputPagesService) {
         this.windowStorage = JSON.parse(window.localStorage.getItem('user'));
         this.token = this.windowStorage.token;
         this.paramCompanyName = this._inputFormService.paramCompanyName;
-        console.log(this.paramCompanyName)
+        // console.log(this.paramCompanyName)
     }
 
-    getData() {
-        this._url = `http://localhost:3000/api/uglist?token=${this.token}&&companyName=${this.paramCompanyName}`;
+    getIncomingData() {
+        this._url = `http://localhost:3000/api/receiptStored?token=${this.token}&&companyName=${this.paramCompanyName}`;
         return this.http.get(this._url);
-    }
-
-    createNewEntry(user: any) {
-
-        this._url = `http://localhost:3000/api/receipt?token=${this.token}&companyName=${this.paramCompanyName}`;
-        return this.http.post(this._url, user)
-            .map((res: Response) => {
-                this.result = res.json();
-                // console.log(this.result)
-            })
     }
 
 }
