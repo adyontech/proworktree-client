f3060171940b15afd1e7389381e15dd74c6387f6
aadii104 <sharmaadi104@gmail.com>
2018-02-11 05:24:15 +0530

    ed6=>es7

--- src/app/pages/outputPages/components/cashAtBank/cashAtBank.component.ts
+++ src/app/pages/outputPages/components/cashAtBank/cashAtBank.component.ts
@@ -1,94 +1,199 @@
-import { Component, Input, ViewChild,  OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormArray, FormBuilder, Validators } from '@angular/forms';
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Component, Input, ViewChild, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormControl,
+  FormArray,
+  FormBuilder,
+  Validators
+} from "@angular/forms";
+import {
+  Router,
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot
+} from "@angular/router";
 
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute } from "@angular/router";
 import { CashAtBankService } from "./service/cashAtBank.service";
-import { IMyDpOptions } from 'mydatepicker';
+import { IMyDpOptions } from "mydatepicker";
 import { BsModalComponent, BsModalBodyComponent } from "ng2-bs3-modal";
 
 declare var $: any;
 
 @Component({
-    selector: 'app-cashAtBank',
-    host: { '(window:keydown)': 'hotkeys($event)' },
-    templateUrl: './cashAtBank.component.html',
-    styleUrls: ['./cashAtBank.component.scss']
+  selector: "app-cashAtBank",
+  host: { "(window:keydown)": "hotkeys($event)" },
+  templateUrl: "./cashAtBank.component.html",
+  styleUrls: ["./cashAtBank.component.scss"]
 })
-
-
 export class CashAtBankComponent implements OnInit {
+  contentId: string = "";
+  public dateFrom: Date;
+  public dateTo: Date;
 
+  @ViewChild("modal") modal: BsModalComponent;
 
-    contentId: string = "";
-    public dateFrom: Date;
-    public dateTo: Date;
+  debSum: number;
+  credSum: number;
+  sumTotal: number;
+  incomingData: Array<string>;
+  form: FormGroup;
+  public dataCopy: any;
+  public paramId: string;
+  public closeResult: string;
+  public ledgerList: Array<string> = [];
 
-    @ViewChild('modal')
-    modal: BsModalComponent;
+  constructor(
+    private route: ActivatedRoute,
+    public _cashAtBankService: CashAtBankService,
+    public fb: FormBuilder
+  ) {}
 
-    incomingData: Array<string>;
-    form: FormGroup;
-    public dataCopy: any;
-    public paramId: string;
-    public closeResult: string;
+  ngOnInit() {
+    this.getLedgerNameData();
 
-    constructor(
-        private route: ActivatedRoute,
-        public _cashAtBankService: CashAtBankService,
-        public fb: FormBuilder, ) {
+    this.modal.onClose.subscribe(this.onClose.bind(this));
+  }
 
+  hotkeys(event) {
+    if (event.keyCode == 76 && event.ctrlKey) {
+      this.modal.open();
     }
+  }
 
-    ngOnInit() {
-        this.getIncomingData();
+  onClose() {
+    console.log("Modal Closed");
+    this.contentId = "";
+  }
+  public myDatePickerOptions: IMyDpOptions = {
+    // other options...
+    dateFormat: "dd.mm.yyyy"
+  };
 
-        this.modal.onClose.subscribe(this.onClose.bind(this));
+  public value: any = {};
+  public _disabledV: string = "0";
+  public disabled: boolean = false;
+  private get disabledV(): string {
+    return this._disabledV;
+  }
 
-    }
+  private set disabledV(value: string) {
+    this._disabledV = value;
+    this.disabled = this._disabledV === "1";
+  }
 
-    hotkeys(event) {
-        if (event.keyCode == 76 && event.ctrlKey) {
+  public selected(value: any): void {
+    this.getIncomingData(value.id);
+  }
 
-            this.modal.open();
-        }
-    }
+  public removed(value: any): void {
+    console.log("Removed value is: ", value);
+  }
 
-    onClose() {
-        console.log('Modal Closed');
-        this.contentId = "";
-    }
-    public myDatePickerOptions: IMyDpOptions = {
-        // other options...
-        dateFormat: 'dd.mm.yyyy',
-    };
+  // public typed(value: any): void {
+  //     console.log('New search input: ', value);
+  // }
 
-    getIncomingData() {
-        this.dataCopy = this._cashAtBankService.getIncomingData().map(
-            (response) => response.json()
-        ).subscribe(
-            (data) => {
-                console.log(data.contraData)
-                this.incomingData = data.contraData;
+  public refreshValue(value: any): void {
+    this.value = value;
+  }
+
+  getLedgerNameData() {
+    this.dataCopy = this._cashAtBankService
+      .getLedgerNameData()
+      .map(response => response.json())
+      .subscribe(data => {
+        this.ledgerList = this.ledgerList.concat(data.ledgerData);
+
+        this.getIncomingData(this.ledgerList[0]);
+      });
+  }
+  getIncomingData(value) {
+    this.dataCopy = this._cashAtBankService
+      .getIncomingData(value)
+      .map(response => response.json())
+      .subscribe(data => {
+        // console.log(data);
+        this.caseThrough(data.formData);
+      });
+  }
+
+  caseThrough(arg) {
+    this.debSum = 0;
+    this.credSum = 0;
+    // console.log(arg);
+    arg.map(el => {
+      switch (el.source.toLowerCase()) {
+        case "payment": {
+          //   console.log(el.data)
+          el.data.map(elm =>
+            elm.particularsData.map(ele => {
+              if (elm.account.toLowerCase() == "cash") {
+                ele["creditAmount"] = ele.amount;
+                this.credSum += ele.amount;
+                ele["debitAmount"] = 0;
+              } else {
+                ele["debitAmount"] = ele.amount;
+                this.debSum += ele.amount;
+                ele["creditAmount"] = 0;
+              }
             })
-    }
+          );
+          break;
+        }
+        case "receipt": {
+          el.data.map(elm =>
+            elm.particularsData.map(ele => {
+              if (elm.account.toLowerCase() == "cash") {
+                ele["debitAmount"] = ele.amount;
+                this.debSum += ele.amount;
+                ele["creditAmount"] = 0;
+              } else {
+                ele["creditAmount"] = ele.amount;
+                this.credSum += ele.amount;
+                ele["debitAmount"] = 0;
+              }
+            })
+          );
+          break;
+        }
+        case "conta": {
+          el.data.map(elm =>
+            elm.particularsData.map(ele => {
+              if (elm.account.toLowerCase() == "cash") {
+                ele["debitAmount"] = ele.amount;
+                this.debSum += ele.amount;
+                ele["creditAmount"] = 0;
+              } else {
+                ele["creditAmount"] = ele.amount;
+                this.credSum += ele.amount;
+                ele["debitAmount"] = 0;
+              }
+            })
+          );
+          break;
+        }
+        case "journal": {
+         var  nameList = el.data.particularsData.map(ele => ele.particulars[0].id);
+          console.log(nameList.map(ele=>ele))
 
-    editData(id) {
-        console.log(id);
-        this.contentId = id;
-        this._cashAtBankService.contentId = id;
+          break;
+        }
+      }
+    });
 
-    }
+    // console.log(arg);
+    this.sumTotal = Math.abs(this.debSum - this.credSum);
+    this.incomingData = arg.map(el => el.data)[0];
+  }
 
-    deleteData(id) {
+  editData(id) {
+    console.log(id);
+    this.contentId = id;
+    this._cashAtBankService.contentId = id;
+  }
 
+  deleteData(id) {}
 
-    }
-
-    copyData(id) {
-
-
-    }
-
-
+  copyData(id) {}
 }
